5fa014c672346307ef8f0237ac3da26d75f7fa03
Aviral <akhanduja_be19@thapar.edu>
2022-04-21 20:00:41 +0530

    debug required

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,7 @@
 exports.display=function(req,res){
     res.render('login');
 };
+module.exports.createSession = function(req, res){
+    console.log("&&& right Place!");
+    return res.redirect('/sign-up');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express=require('express');
 const manipulator=require('./manipulation');
+const passport=require('passport');
 const router=express.Router();
 const homeController=require('../controllers/homeController');
 router.get('/',homeController.display);
 router.use('/sign-up',manipulator);
-module.exports=router;
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/'},
+), homeController.createSession);
+
+module.exports = router;
